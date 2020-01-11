b8b9e2ac5b2aace6cfbd49980b2bd5158d67ea54
TNS Monty <Syndyr@syndyr@maimedpixel.xyz>
2020-01-11 12:57:56 +0000

    Initial commit

---
+++ main.lua
@@ -0,0 +1,38 @@
+function love.load()
+end
+
+function love.draw()
+end
+
+function love.update(dt)
+end
+
+function love.resize(x,y)
+end
+
+function love.focus(bool)
+end
+
+function love.textinput(t)
+end
+
+function love.keypressed( key, unicode )
+end
+
+function love.visible(visible)
+end
+
+function love.keyreleased( key, unicode )
+end
+
+function love.mousepressed( x, y, button )
+end
+
+function love.wheelmoved( x, y )
+end
+
+function love.mousereleased( x, y, button )
+end
+
+function love.quit()
+end
